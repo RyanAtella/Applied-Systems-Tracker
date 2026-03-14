58ec95d71e74bde0b4d45f1cae9bed80a8e991ac
Rza7199 <ryan.atella@uri.edu>
2026-03-14 18:52:02 -0400

    More CORS debugging

--- app/main.py
+++ app/main.py
@@ -5,9 +5,12 @@ from app.database import get_db
 from app.schemas import UserCreate, UserResponse, ApplicationCreate, ApplicationResponse
 from app import models, schemas
 from fastapi.middleware.cors import CORSMiddleware
+from models import Base
+from database import engine
 import logging
 import sys
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.add_middleware(
